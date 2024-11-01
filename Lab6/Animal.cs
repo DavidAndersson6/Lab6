2b3ac3ea89021c8df79ab15047cc327ac507e933
admin <admin@DESKTOP-KVNK9SN>
2024-11-01 16:28:38 +0100

    Add project files.

---
+++ Lab6/Animal.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Animal
+    {
+
+        public int Legs { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+
+
+        public Animal(int legs, string name, string color, string species, int age)
+        {
+            Legs = legs;
+            Name = name;
+            Color = color;
+            Species = species;
+            Age = age;
+        }
+
+        public void PrintNumberOfLegs()
+        {
+            Console.WriteLine($"Number of legs of {Name} is {Legs}");
+        }
+
+        public void PrintColor()
+        {
+            Console.WriteLine($"Color of {Name} is {Color}");
+        }
+
+        public void PrintAge()
+        {
+            Console.WriteLine($"Age of {Name} is {Age}");
+        }
+    }
+
+     public class Dog : Animal
+
+    {
+        public bool Drools { get; set; }
+        public Dog(int legs, string name, string color, string species, int age, bool drools)
+        : base(legs, name, color, species, age)
+        {
+            Drools = drools;
+        }
+
+        public virtual void MakeSound()
+        {
+
+            Console.WriteLine($"{Name} says Woof");
+        }
+    }
+
+    public class Cat : Animal
+
+    {
+        public bool Whiskers { get; set; }
+        public Cat(int legs, string name, string color, string species, int age, bool whiskers)
+        : base(legs, name, color, species, age)
+        {
+            Whiskers = whiskers;
+        }
+        public void MakeSound()
+        {
+
+            Console.WriteLine($"{Name} says Mjau");
+        }
+    }
+
+    public class Fish : Animal
+
+    {
+        public bool Predator;
+        public Fish(int legs, string name, string color, string species, int age, bool predator)
+        : base(legs, name, color, species, age)
+        {
+            Predator = predator; 
+        }
+        public void MakeSound()
+        {
+
+            Console.WriteLine($"{Name} says blubb");
+        }
+    }
+
+    public class Bulldog : Dog
+    {
+        public bool Aggressive { get; set; }
+        public Bulldog(int legs, string name, string color, string species, int age, bool drools, bool aggressive)
+       : base(legs, name, color, species, age, drools)
+        {
+            Aggressive = aggressive;
+        }
+            public override void MakeSound()
+        {
+            base.MakeSound();
+            if (Aggressive)
+            {
+                Console.WriteLine($"{Name} growls aggressively!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} barks in a friendly way!");
+            }
+        }
+
+
+    }
+
+    public class Chihuahua : Dog
+    {
+        public bool HasHair { get; set; }
+        public Chihuahua(int legs, string name, string color, string species, int age, bool drools, bool hasHair)
+       : base(legs, name, color, species, age, drools)
+        {
+            HasHair = hasHair;
+        }
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            if (HasHair)
+            {
+                Console.WriteLine($"{Name} growls happily because it has hair!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} barks in a sad way because it has no hair :( ");
+            }
+        }
+
+
+    }
+
+}
+
---
+++ Lab6/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dog myDog = new Dog(4, "Buddy", "Brown", "Golden Retriever", 3, true);
+
+            Cat myCat = new Cat(4, "Misse", "Gray", "Norwegian Forest Cat", 5, true);
+
+            Fish myFish = new Fish(0, "Blubber", "Orange", "Clownfish", 2, false);
+
+            Bulldog myBulldog = new Bulldog(4, "Fido", "Black", "French Bulldog", 3, false, true);
+
+            Chihuahua myChihuahua = new Chihuahua(4, "Lillen", "Orange", "Pomerian", 6, false, true);
+
+            myDog.PrintNumberOfLegs();
+
+            myCat.PrintColor();
+
+            myFish.PrintAge();
+
+            myDog.MakeSound();
+
+            myCat.MakeSound();
+
+            myFish.MakeSound();
+
+            myBulldog.MakeSound();
+
+            myChihuahua.MakeSound();
+        }
+    }
+}
